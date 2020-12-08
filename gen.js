3461f716375ae24814320cb1ee3c1e20c8d5c981
Tuan Nguyen <tuannguyen20071844@gmail.com>
2020-12-08 11:54:27 -0500

    Added parameters for editting boards, and a draw mechanism

--- gen.js
+++ gen.js
@@ -1,20 +1,62 @@
 window.onload = genBoards;
 
-var min = 1;
-var max = 30;
-var size = 4;
-var nBoards = 3;
+let min;
+let max;
+let size;
+let nBoards;
+let draws;
+let drawInd;
 
 function genBoards() {
-	// get div with boards
-	let boards = document.getElementById("boards");
+	// read input
+	min = parseInt(document.getElementById("min").value);
+	max = parseInt(document.getElementById("max").value);
+	size = parseInt(document.getElementById("size").value);
+	nBoards = parseInt(document.getElementById("nBoards").value);
+	draws = shuffleNums();
+	drawInd = null;
 
-	// clear previous boards
-	boards.innerHTML = "";
+	// check if there's enough numbers for bingo board
+	if((max - min + 1) < size*size) {
+		alert("Board size too large for given number range.\nIncrease the number range or decrease board size.")
+	} else {
+		// get div with boards
+		let boards = document.getElementById("boards");
 
-	// fill with boards
-	for(let i = 0; i < nBoards; i++) {
-		boards.appendChild(newBoard(i));
+		// clear previous boards
+		boards.innerHTML = "";
+
+		// fill with boards
+		for(let i = 0; i < nBoards; i++) {
+			boards.appendChild(newBoard(i));
+		}
+	}
+}
+
+function draw() {
+	if(drawInd == null) {
+		drawInd = 0;
+		document.getElementById("drawNum").innerHTML = draws[drawInd];
+		document.getElementById("pNum").value = "";
+		document.getElementById("mNum").value = "";
+	} else if(drawInd < draws.length - 1) {
+		drawInd += 1;
+		document.getElementById("drawNum").innerHTML = draws[drawInd];
+		document.getElementById("pNum").value = "";
+		document.getElementById("mNum").value = "";
+	} else {
+		alert("Out of numbers.");
+	}
+}
+
+function back() {
+	if(drawInd == 0 || drawInd == null) {
+		alert("Can't go back.")
+	} else {
+		drawInd -= 1;
+		document.getElementById("drawNum").innerHTML = draws[drawInd];
+		document.getElementById("pNum").value = "";
+		document.getElementById("mNum").value = "";
 	}
 }
 
@@ -25,12 +67,13 @@ function newBoard(nBoard) {
 
 	// add text box for student's name
 	let name = document.createElement("INPUT");
-	name.setAttribute("type", "text");
+	name.className = "name";
 	name.style.width = (size*56+2) + "px";
+	name.setAttribute("type", "text");
 	board.appendChild(name);
 
-	// store used numbers
-	let usedNums = new Array(max+1);
+	// shuffle number range
+	let nums = shuffleNums();
 
 	// start a <table> node
 	let tab = document.createElement("TABLE");
@@ -41,16 +84,12 @@ function newBoard(nBoard) {
 
 		// fill row with numbers
 		for(let j=0; j < size; j++) {
-			// generate a random number that has not been used yet
-			let num = Math.floor(Math.random() * (max - min + 1)) + min;
-			do {
-				num = Math.floor(Math.random() * (max - min + 1)) + min;
-			} while(usedNums[num]);
-
-			usedNums[num] = true;
+			// pick a number from the shuffled list in order
+			let num = nums[i*size + j]
 
 			// fill row with number
 			let cell = document.createElement("TH");
+			cell.className = "cell";
 			cell.setAttribute("id", "cell" + nBoard.toString() + i.toString() + j.toString())
 			cell.addEventListener("click", function() { toggle(nBoard, i, j) } );
 			cell.appendChild(document.createTextNode(num));
@@ -68,10 +107,34 @@ function newBoard(nBoard) {
 }
 
 function toggle(nBoard, i, j) {
+	// find the cell that was clicked
 	let cell = document.getElementById("cell" + nBoard.toString() + i.toString() + j.toString());
+
+	// toggle cell color
 	if(cell.style.backgroundColor == "") {
 		cell.style.backgroundColor = "DimGray";
 	} else {
 		cell.style.backgroundColor = "";
 	}
+}
+
+function shuffleNums() {
+	// create array with all numbers in range
+	let nums = [...Array(max + 1).keys()].slice(min);
+
+	// shuffle array
+	let currInd = nums.length;
+	let randInd;
+	let temp;
+
+	while(currInd != 0) {
+		// pick a random index to switch with the current index
+		randInd = Math.floor(Math.random() * currInd);
+		currInd -= 1;
+		temp = nums[currInd];
+		nums[currInd] = nums[randInd];
+		nums[randInd] = temp;
+	}
+
+	return nums;
 }
